da4520645f7bc403bdc104ef21bffbc42d081c0a
Darlan Parreiras Santos <darlanparreiras@gmail.com>
2021-06-27 22:13:19 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/com/darlansantos/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.darlansantos.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
